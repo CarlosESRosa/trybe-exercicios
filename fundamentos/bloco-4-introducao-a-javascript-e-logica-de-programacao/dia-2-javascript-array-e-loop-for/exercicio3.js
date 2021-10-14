224efd52b792950107357a9bb22c190304815cd2
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-13 22:23:59 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio3.js
@@ -1,8 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-imprimeValores(numbers);
-function imprimeValores(array){
-    for(var i = 0; i < array.length; i++){
-        console.log(array[i]);
+function somaValores(array){
+    var somaDosValores = array[0];
+    for(var i = 1; i < array.length; i++){
+        somaDosValores += array[i];
     }
+    return somaDosValores;
 }
+
+function mediaAritmetica (soma){
+    var media = soma/numbers.length;
+    return media;
+}
+
+console.log(mediaAritmetica(somaValores(numbers)));
