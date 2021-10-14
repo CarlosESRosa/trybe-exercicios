df81888c8df90a3be00faf847f8dce79d2066dab
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-13 22:38:13 -0300

    Exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
@@ -1,8 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-imprimeValores(numbers);
-function imprimeValores(array){
+function imprimeImpar(array){
+    var count = 0;
+
     for(var i = 0; i < array.length; i++){
-        console.log(array[i]);
+        if(array[i] % 2 != 0){
+            count++;
+        }
     }
+    if(count == 0){
+        return "nenhum valor ímpar encontrado";
+    }
+    return count;
 }
+
+console.log(imprimeImpar(numbers));
