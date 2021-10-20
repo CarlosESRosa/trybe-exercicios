8180ba284d1684dbe83a05141b2a4811de630f68
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-19 22:48:15 -0300

    Exercicio 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -3,3 +3,9 @@ function changeTextP() {
   document.getElementsByTagName("p")[0].innerText =
     "Trabalhando como Engenheiro de Software";
 }
+
+changeColorBox();
+function changeColorBox() {
+  document.getElementsByClassName("main-content")[0].style.backgroundColor =
+    "rgb(76,164,109)";
+}
