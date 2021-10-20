b4e9983bfb60c8518a8b2f9fd8e46fc022a75bde
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-19 22:51:11 -0300

    Exercicio 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -9,3 +9,9 @@ function changeColorBox() {
   document.getElementsByClassName("main-content")[0].style.backgroundColor =
     "rgb(76,164,109)";
 }
+
+changeColorCenterBox();
+function changeColorCenterBox() {
+  document.getElementsByClassName("center-content")[0].style.backgroundColor =
+    "white";
+}
