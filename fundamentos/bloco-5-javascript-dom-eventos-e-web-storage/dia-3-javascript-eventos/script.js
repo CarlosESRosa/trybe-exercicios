eacf3dc28c18041bfb8dc375d90c8d7c11cb4de2
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-21 21:10:36 -0300

    Exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,56 @@
+function createDaysOfTheWeek() {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+function createDaysOfMonth() {
+  const dezDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+  const monthDaysList = document.querySelector("#days"); //pegando a ul do html
+
+  for (let index = 0; index < dezDaysList.length; index++) {
+    const daysOfMonth = dezDaysList[index];
+    const dayMonthListItem = document.createElement("li"); //criando as li
+
+    //atribuindo classes
+    dayMonthListItem.className = "day";
+    if (daysOfMonth == 24 || daysOfMonth == 25 || daysOfMonth == 31) {
+      dayMonthListItem.className = "day holiday";
+    }
+    if (
+      daysOfMonth == 4 ||
+      daysOfMonth == 11 ||
+      daysOfMonth == 18 ||
+      daysOfMonth == 25
+    ) {
+      dayMonthListItem.className = "day friday";
+    }
+
+    dayMonthListItem.innerHTML = daysOfMonth; // passando o valor de daysOfMonth para a li
+
+    monthDaysList.appendChild(dayMonthListItem);
+  }
+}
+createDaysOfMonth();
