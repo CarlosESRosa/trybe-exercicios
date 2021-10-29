324740fca66dbcc074bc05b71243c6a025a9a7e6
Carlos Eduardo <carlosrosa1001@gmail.com>
2021-10-29 14:53:40 -0300

    Comecando form

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,39 @@
+const idEstados = document.querySelector("#estado");
+
+var estados = {
+  AC: "Acre",
+  AL: "Alagoas",
+  AP: "Amapá",
+  AM: "Amazonas",
+  BA: "Bahia",
+  CE: "Ceará",
+  DF: "Distrito Federal",
+  ES: "Espírito Santo",
+  GO: "Goiás",
+  MA: "Maranhão",
+  MT: "Mato Grosso",
+  MS: "Mato Grosso do Sul",
+  MG: "Minas Gerais",
+  PA: "Pará",
+  PB: "Paraíba",
+  PR: "Paraná",
+  PE: "Pernambuco",
+  PI: "Piauí",
+  RJ: "Rio de Janeiro",
+  RN: "Rio Grande do Norte",
+  RS: "Rio Grande do Sul",
+  RO: "Rondônia",
+  RR: "Roraima",
+  SC: "Santa Catarina",
+  SP: "São Paulo",
+  SE: "Sergipe",
+  TO: "Tocantins",
+};
+
+for (var key in estados) {
+  const option = document.createElement("option");
+  option.value = key;
+  option.innerText = estados[key];
+  idEstados.appendChild(option);
+  // console.log(key + " = " + estados[key]);
+}
