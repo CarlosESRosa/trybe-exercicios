9e63f3947c273caca52ec313ec46da2baa8f28ed
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-06-02 12:15:54 -0300

    add new var

--- back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/index.js
+++ back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 // exercicio 1
-app.get('/', (req, res) => res.send("Está vivo!!!"))
+const MY_VAR = process.env.MY_VAR;
+app.get('/', (req, res) => res.send(`Está vivo!!! ${MY_VAR}`))
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
