75202eb721c5197036bea486985f2614315a70dc
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-06-22 16:57:45 -0300

    26.2 exercise 4

--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/build/exercise3.js
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/build/exercise3.js
@@ -5,3 +5,5 @@ const nutela = { flavor: 'Nutela', slices: 4 };
 console.log(calabresa);
 console.log(marguerita);
 console.log(nutela);
+const calabresa2 = { flavor: 'Calabresa', slices: 6 };
+console.log(calabresa2);
--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/exercise3.ts
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/exercise3.ts
@@ -12,3 +12,20 @@ const nutela: IPizza = {flavor:'Nutela', slices: 4};
 console.log(calabresa);
 console.log(marguerita);
 console.log(nutela);
+
+interface pizzaComum extends IPizza {
+  flavor: 'Calabresa' | 'Frango' | 'Pepperoni',
+}
+
+interface pizzaVegetariana extends IPizza {
+  flavor: 'Marguerita' | 'Palmito' | 'Cogumelo',
+}
+
+interface pizzaDoce extends IPizza {
+  flavor: 'Nutela' | 'Goiabada com Queijo' | 'Marshmallow';
+  slices: 4;
+}
+
+const calabresa2: pizzaComum = {flavor: 'Calabresa', slices: 6};
+
+console.log(calabresa2);
