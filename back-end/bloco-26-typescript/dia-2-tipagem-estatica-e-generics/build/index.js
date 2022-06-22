c106e1b9779d35ec79b2511c9be8db949a0a0a06
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-06-22 16:43:31 -0300

    26.2 exercicio 2

--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/build/index.js
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/build/index.js
@@ -6,13 +6,20 @@ var color;
     color["vermelha"] = "vermelha";
     color["prata"] = "prata";
 })(color || (color = {}));
+var direction;
+(function (direction) {
+    direction["RIGHT"] = "right";
+    direction["LEFT"] = "left";
+    direction["CENTER"] = "center";
+})(direction || (direction = {}));
 class Car {
-    constructor(brand, color, doors, isTurnedOn, speed) {
+    constructor(brand, color, doors, isTurnedOn, speed, direction) {
         this.brand = brand;
         this.color = color;
         this.doors = doors;
         this.isTurnedOn = isTurnedOn;
         this.speed = speed;
+        this.carDirection = direction;
     }
     honk() {
         console.log('acionando BUZINA BUMMMMMMMMMMMMMMMMMMMMMMMMMMM');
@@ -41,11 +48,26 @@ class Car {
     turn() {
         console.log('virou o carro');
     }
+    turnLeft() {
+        this.carDirection = direction['LEFT'];
+    }
+    turnRight() {
+        this.carDirection = direction['RIGHT'];
+    }
 }
-const myCar = new Car('Mercedes', color['branca'], 4, false, 0);
+/*
+const myCar = new Car('Mercedes', color['branca'], 4, false, 0, direction['CENTER']);
 console.log(myCar);
 myCar.turnOn();
 myCar.speedUp();
-myCar.speedUp();
-myCar.speedUp();
 console.log(myCar);
+*/
+const uberCar = new Car('volkswagen', color['prata'], 4, false, 0, direction['CENTER']);
+uberCar.turnOn();
+uberCar.turnLeft();
+uberCar.turnLeft();
+uberCar.turnRight();
+uberCar.turnRight();
+uberCar.turnRight();
+uberCar.turnOff();
+console.log(uberCar);
--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/index.ts
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/src/index.ts
@@ -4,6 +4,11 @@ enum color {
   vermelha = 'vermelha',
   prata = 'prata'
 }
+enum direction {
+  RIGHT = 'right',
+  LEFT = 'left',
+  CENTER = 'center'
+}
 
 class Car {
   brand: string;
@@ -11,13 +16,15 @@ class Car {
   doors: number;
   isTurnedOn: boolean;
   speed: number;
+  carDirection: direction;
 
-  constructor(brand: string, color: color, doors: number, isTurnedOn: boolean, speed: number){
+  constructor(brand: string, color: color, doors: number, isTurnedOn: boolean, speed: number, direction: direction){
     this.brand = brand;
     this.color = color;
     this.doors = doors;
     this.isTurnedOn = isTurnedOn;
     this.speed = speed;
+    this.carDirection = direction;
   }
 
   honk(): void {
@@ -55,13 +62,32 @@ class Car {
   turn(): void {
     console.log('virou o carro');
   }
+
+  turnLeft(): void {
+    this.carDirection = direction['LEFT']
+  }
+
+  turnRight(): void {
+    this.carDirection = direction['RIGHT']
+  }
 }
 
-const myCar = new Car('Mercedes', color['branca'], 4, false, 0);
-
+/* 
+const myCar = new Car('Mercedes', color['branca'], 4, false, 0, direction['CENTER']);
 console.log(myCar);
 myCar.turnOn();
 myCar.speedUp();
-myCar.speedUp();
-myCar.speedUp();
 console.log(myCar);
+*/
+
+const uberCar = new Car('volkswagen', color['prata'], 4, false, 0, direction['CENTER']);
+
+uberCar.turnOn();
+uberCar.turnLeft();
+uberCar.turnLeft();
+uberCar.turnRight();
+uberCar.turnRight();
+uberCar.turnRight();
+uberCar.turnOff();
+console.log(uberCar);
+ 
